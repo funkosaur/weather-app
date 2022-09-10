013e54701a956a33702ce5b9d798a3fb7f1481bb
funkosaur <mario.bojkovski@outlook.com>
2022-09-10 04:50:04 +0100

    made a changeMain js file moved functions there and finished the changeWeatherIcon function

---
+++ src/functions/changeMainWeatherFunctions.js
@@ -0,0 +1,32 @@
+function changeCityTemperature(weatherData) {
+  const mainTemperature = document.querySelector('.currentTemperature');
+  const highTemperature = document.querySelector('#highTemp');
+  const lowTemperature = document.querySelector('#lowTemp');
+  const feelsLikeTemperature = document.querySelector('.feelsLikeDiv');
+
+  console.log(weatherData);
+
+  mainTemperature.textContent = `${Math.floor(weatherData.current.temp)}°`;
+  highTemperature.textContent = `High: ${Math.floor(
+    weatherData.daily[0].temp.max,
+  )}°`;
+  lowTemperature.textContent = `Low: ${Math.floor(
+    weatherData.daily[0].temp.min,
+  )}°`;
+  feelsLikeTemperature.textContent = `Feels Like: ${Math.floor(
+    weatherData.current.feels_like,
+  )}°`;
+}
+
+function changeCityName(weatherData) {
+  const cityName = document.querySelector('#cityName');
+  cityName.textContent = `${weatherData.name}, ${weatherData.country}`;
+}
+
+function changeWeatherIcon(weatherData) {
+  const weatherIcon = document.querySelector('.weatherIcon');
+  console.log(weatherData.current.weather[0].icon);
+  weatherIcon.src = `https://openweathermap.org/img/wn/${weatherData.current.weather[0].icon}@2x.png`;
+}
+
+export { changeCityTemperature, changeCityName, changeWeatherIcon };
--- src/functions/eventListeners.js
+++ src/functions/eventListeners.js
@@ -1,36 +1,12 @@
 import getWeatherForLocation from './apiFunctions';
+import {
+  changeCityTemperature,
+  changeCityName,
+  changeWeatherIcon,
+} from './changeMainWeatherFunctions';
 
 const searchBar = document.querySelector('#searchBar');
 const searchButton = document.querySelector('.searchButton');
-
-function changeCityTemperature(weatherData) {
-  const mainTemperature = document.querySelector('.currentTemperature');
-  const highTemperature = document.querySelector('#highTemp');
-  const lowTemperature = document.querySelector('#lowTemp');
-  const feelsLikeTemperature = document.querySelector('.feelsLikeDiv');
-
-  console.log(weatherData);
-
-  mainTemperature.textContent = `${Math.floor(weatherData.current.temp)}°`;
-  highTemperature.textContent = `High: ${Math.floor(
-    weatherData.daily[0].temp.max,
-  )}°`;
-  lowTemperature.textContent = `Low: ${Math.floor(
-    weatherData.daily[0].temp.min,
-  )}°`;
-  feelsLikeTemperature.textContent = `Feels Like: ${Math.floor(
-    weatherData.current.feels_like,
-  )}°`;
-}
-
-function changeCityName(weatherData) {
-  const cityName = document.querySelector('#cityName');
-  cityName.textContent = `${weatherData.name}, ${weatherData.country}`;
-}
-
-function changeWeatherIcon(weatherData) {
-  const weatherIcon = document.querySelector('.weatherIcon');
-}
 
 searchButton.addEventListener('click', () => {
   if (searchBar.value === '') {
@@ -39,6 +15,7 @@ searchButton.addEventListener('click', () => {
   getWeatherForLocation(searchBar.value).then((currentWeatherData) => {
     changeCityTemperature(currentWeatherData);
     changeCityName(currentWeatherData);
+    changeWeatherIcon(currentWeatherData);
   });
   searchBar.value = '';
 });
