6a4973eae6963d0be2981aed27efad803532751d
funkosaur <mario.bojkovski@outlook.com>
2022-09-28 07:28:19 +0100

    another mobile keyboard viewport fix

--- src/index.js
+++ src/index.js
@@ -2,3 +2,11 @@ import './styles/style.scss';
 import renderWeather from './functions/eventListeners';
 
 renderWeather('London');
+
+window.addEventListener('load', () => {
+  const viewport = document.querySelector('meta[name=viewport]');
+  viewport.setAttribute(
+    'content',
+    `${viewport.content}, height=${window.innerHeight}`,
+  );
+});
